d7abbec29f032a4fb815fc284cb8e3685c15b807
BypassTheFedR <94949817+BypassTheFedR@users.noreply.github.com>
2026-04-18 16:05:29 -0700

    Changed to Mapped Columns version

--- app/database/tables.py
+++ app/database/tables.py
@@ -1,28 +1,30 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 Base = declarative_base()
 
 class Ingredient(Base):
     __tablename__ = "ingredients"
-    id = Column("id", Integer, primary_key=True)
-    name = Column("name", String, nullable=False)
-    serving_size = Column("serving_size", Float)
-    serving_units = Column("serving_units", String)
-    cal = Column("calories", Integer)
-    protein = Column("protein", Integer)
-    fat = Column("fat", Integer)
-    carbs = Column("carbs", Integer)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    serving_size: Mapped[str] = mapped_column()
+    serving_units: Mapped[str] = mapped_column()
+    cal: Mapped[int] = mapped_column()
+    protein: Mapped[int] = mapped_column()
+    fat: Mapped[int] = mapped_column()
+    carbs: Mapped[int] = mapped_column()
 
 class RecipeIngredient(Base):
     __tablename__ = "recipe_ingredients"
 
-    id = Column("id", Integer, primary_key=True)
-    recipe_id = Column("recipe_id", Integer, ForeignKey("recipes.id"), nullable=False)
-    ingredient_id = Column("ingredient_id", Integer, ForeignKey("ingredients.id"), nullable=False)
-    amount = Column("amount", Float, nullable=False)
-    units = Column("units", String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    recipe_id: Mapped[int] = relationship(back_populates="recipes", nullable=False)
+    ingredient_id: Mapped[int] = relationship(back_populates="ingredients", nullable=False)
+    amount: Mapped[str] = mapped_column(nullable=False)
+    units: Mapped[str] = mapped_column(nullable=False)
 
 class RecipeStep(Base):
     __tablename__ = "recipe_steps"
